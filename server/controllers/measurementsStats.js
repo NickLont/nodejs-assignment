d49721f0628f02495f0d16acf0dbac3a20aeb7bf
NickLont <tormentor_gr@hotmail.com>
2019-11-08 15:50:25 +0100

    time limiting results working

--- server/controllers/measurementsStats.js
+++ server/controllers/measurementsStats.js
@@ -6,6 +6,7 @@ const Vehicle = require('../models/vehicle')
 exports.allMeasurements = async (req, res) => {
   const { vehicleId, vehicleName, startDate, endDate } = req.query
   let measurements
+  let vehicle
 
   if (!vehicleId && !vehicleName) return res.status(401).send('Please provide a vehicleId or a vehicleName')
   if (vehicleId && vehicleName) return res.status(401).send('Please provide one of vehicleId or vehicleName')
@@ -13,19 +14,35 @@ exports.allMeasurements = async (req, res) => {
   if (vehicleId) {
     if (!mongoose.Types.ObjectId.isValid(vehicleId)) return res.status(401).send('Invalid ID')
 
-    const vehicle = await Vehicle.findById(vehicleId)
+    vehicle = await Vehicle.findById(vehicleId)
     if (!vehicle) return res.status(401).send('No such vehicle ID found')
-
-    measurements = await Measurements.find({ vehicle: vehicle }, { _id: 0 }).populate('vehicle')
   }
   if (vehicleName) {
-    const vehicle = await Vehicle.findOne({ name: vehicleName })
+    vehicle = await Vehicle.findOne({ name: vehicleName })
     if (!vehicle) return res.status(401).send('No such vehicle name found')
-    measurements = await Measurements
-      .find({ vehicle: vehicle, time: { $gte: new Date(1511437632000), $lt: new Date(1511437633000) } }, { _id: 0 })
-      // .find({ vehicle: vehicle, time: { $gte: new Date(1511437632000), $lt: new Date(1511437633000) } }, { _id: 0 })
-      .populate('vehicle')
   }
-  res.status(200).send(measurements)
-  // 1511436602000
+
+  let options = {
+    vehicle: vehicle
+  }
+  // Conditionally creating { vehicle: vehicle, time: { $gte: new Date(1511437632000), $lt: new Date(1511437633000) }
+  let time = {}
+  if (startDate && endDate) {   
+    time = {$gte: Number(startDate), $lt: Number(endDate)}
+  }
+  if (startDate && !endDate) {   
+    time = {$gte: Number(startDate)}
+  }
+  if (!startDate && endDate) {   
+    time = {$lt: Number(endDate)}
+  }
+  if (startDate || endDate) {
+    options = {
+      ...options,
+      time
+    }
+  }
+
+  measurements = await Measurements.find(options, { _id: 0 }).populate('vehicle')
+  return res.status(200).send(measurements)
 }
--- server/routes/measurementsStats.js
+++ server/routes/measurementsStats.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const measurementsController = require('../controllers/measurementsStats')
 
-router.get('/all', measurementsController.allMeasurements)
+router.get('/', measurementsController.allMeasurements)
 
 module.exports = router
